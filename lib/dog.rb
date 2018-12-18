d2ad8b7ec502a7c43ab7b708293b221d9c112ef1
Tara Sabella <tarasabella@gmail.com>
2018-12-18 23:35:49 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,7 +3,7 @@ class Dog
   attr_accessor :name 
   @@all = []
   #class variable for storing all of the class instances of Dog 
- 
+ #stores each instance of a Dog that is created
   def initialize(name)
     @name = name 
 end 
