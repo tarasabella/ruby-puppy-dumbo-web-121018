e6379a34f72cc66422ca69f2581e553e43c108e0
Tara Sabella <tarasabella@gmail.com>
2018-12-18 23:50:53 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,25 @@
+class Dog 
+  
+  attr_accessor :name 
+  @@all = []
+  
+  #class variable for storing all of the class instances of Dog 
+ #stores each instance of a Dog that is created
+  
+  def initialize(name)
+    @name = name 
+    
+    @@all << self 
+    #pushed each new instance created to the array 
+end 
+
+def self.all 
+  @@all.map do |dog|
+    puts dog.name
+end 
+end 
+
+def self.clear_all
+  @@all.clear 
+end 
+end 
